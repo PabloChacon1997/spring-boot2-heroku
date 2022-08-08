248366c9038e2afdd629192c99e15709edafdf47
PabloChacon1997 <pabloandres120297@gmail.com>
2022-08-08 17:37:12 -0500

    Creando backend

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,43 @@
+package com.bolsadeideas.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.backend.apirest.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+
+
+@Service
+public class ClienteServiceImpl implements IClienteService{
+	
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return (List<Cliente>) clienteDao.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		// TODO Auto-generated method stub
+		return clienteDao.findById(id).orElse(null);
+	}
+	
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		// TODO Auto-generated method stub
+		return clienteDao.save(cliente);
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
+		
+	}
+	
+}
